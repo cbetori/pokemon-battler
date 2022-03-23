c3464c1052be2ccb735e726ff159b5c41cd15990
cbetori <cbetori@gmail.com>
2022-03-23 18:02:03 -0500

    update go vendor

--- vendor/routes/routes.go
+++ vendor/routes/routes.go
@@ -1,28 +1,27 @@
 package routes
 
 import (
-	// "encoding/json"
-	// "io/ioutil"
 	"fmt"
 	"net/http"
-	 "github.com/mtslzr/pokeapi-go"
+
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/mtslzr/pokeapi-go"
 )
 
 func Start(r *http.Request) string {
 	return "API Running"
 }
 
-type PokemonList struct{
-	Id		int 		`json:"id"`
-	Name 	string		`json:"name"`
-	Sprite	string		`json:"sprite"`
+type PokemonList struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Sprite string `json:"sprite"`
 }
 
 func PokemonPicker() {
 	r, err := pokeapi.Resource("pokemon", 0, 151)
 	if err != nil {
-			fmt.Println(err) 
+		fmt.Println(err)
 	}
 	fmt.Println(r.Results[0])
 }
